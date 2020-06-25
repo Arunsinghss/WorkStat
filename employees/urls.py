19bd0c52febb4cee16950c19bc529e89d3ab840f
Arun <arun@care24.co.in>
2020-06-25 10:41:27 +0530

    First commit

---
+++ employees/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from employees import views
+from employees import viewsets as restapis
+
+router = routers.DefaultRouter()
+router.register(r'user', restapis.UserViewset)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
